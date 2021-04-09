131a1ae7611d413bf2a4b07b63ad13cfe542d7a6
mnmm <mnmm@mnmmui-MacBookPro.local>
2021-04-09 17:29:38 +0900

    :fire: practice redux
    리덕스로 카운트 처리하기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
-  return (
-    <div>
-      hi
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div>
+//       hi
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+// TODO: vanilla-redux
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './exercise';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// // import './exercise';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+
+// TODO: --------------------- Vanilla Redux
+
+import { createStore } from 'redux';
+
+const add = document.getElementById('add');
+const minus = document.getElementById('minus');
+const number = document.querySelector('span');
+
+// let count = 0;
+
+// number.innerText = count;
+
+// const updateNumber = () => {
+//   number.innerText = count;
+// }
+
+// const handleAdd = () => {
+//   console.log('add');
+//   count++;
+//   updateNumber();
+// }
+
+// const handleMinus = () => {
+//   console.log('minus');
+//   count--;
+//   updateNumber();
+// }
+
+// add.addEventListener("click", handleAdd);
+// minus.addEventListener("click", handleMinus);
+
+number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  // if(action.type === "ADD") {
+  //   return count + 1;
+  // } else if(action.type === "MINUS") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+
+  switch(action.type) {
+    case ADD: 
+      return count + 1;
+    case MINUS:
+      return count - 1;
+
+    default:
+      return count
+  }
+};
+
+const countStore = createStore(countModifier);
+
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "ADD" });
+// countStore.dispatch({ type: "MINUS" });
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+}
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+}
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
+
+
+// TODO: Vanilla Redux ------------------------
